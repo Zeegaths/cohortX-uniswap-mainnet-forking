7569595868a037f16742adfcd50f3a62208eb4cf
Zeegaths <zarahgathoni76@gmail.com>
2024-02-24 15:39:09 +0100

    eth

--- contracts/IUniswap.sol
+++ contracts/IUniswap.sol
@@ -33,4 +33,14 @@ interface IUniswap{
         address to,
         uint deadline
     ) external returns (uint amountA, uint amountB, uint liquidity);
+
+
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
 }
---
+++ scripts/pancake.ts
@@ -0,0 +1,10 @@
+import { ethers } from "hardhat";
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+
+const main = async () => {
+    const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+    const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const wethAdress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+
+    const PSRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+}
